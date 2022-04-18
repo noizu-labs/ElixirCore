c64e2e741c99a3e1e4c15709762a967bf49e0e02
Keith Brings <keith.brings@noizu.com>
2022-04-18 09:24:19 +0700

    Adding Slim Options protocol, to convert Options to KeyWord dict for metaprogramming

--- lib/elixir_core/options/osp.ex
+++ lib/elixir_core/options/osp.ex
@@ -9,3 +9,27 @@ defprotocol Noizu.ElixirCore.OSP do
   """
   def extract(this, acc)
 end # end defprotocol Noizu.ERP
+
+defprotocol Noizu.ElixirCore.SlimOptions do
+  @fallback_to_any true
+  def slim(entity)
+end
+
+defimpl Noizu.ElixirCore.SlimOptions, for: Map do
+  def slim(entity) do
+    Enum.map(entity, fn({k,v}) -> {k, Noizu.ElixirCore.SlimOptions.slim(v)}   end)
+  end
+end
+
+defimpl Noizu.ElixirCore.SlimOptions, for: Any do
+  def slim(entity) do
+    entity
+  end
+end
+
+defimpl Noizu.ElixirCore.SlimOptions, for: [Noizu.ElixirCore.OptionSettings, Noizu.ElixirCore.OptionValue, Noizu.ElixirCore.OptionList]  do
+  def slim(entity) do
+    Noizu.ElixirCore.SlimOptions.slim(Map.from_struct(entity))
+  end
+end
+
